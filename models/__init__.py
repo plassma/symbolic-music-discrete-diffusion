89b64e3e7839204e2932e6540c76e55d06bbe712
plassma <matthias.plasser@gmail.com>
2023-05-16 15:26:20 +0200

    update sampler gui, refactor 1

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .transformer import Transformer
 from .absorbing_diffusion import AbsorbingDiffusion
 from .conv_transformer import ConVormer
+from .hierarch_transformer import HierarchTransformer
+from .UTransformer import UTransformer
